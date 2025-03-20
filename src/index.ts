cd09695da5b4ca81dd9ea9f57456fb7e1e351cc9
Gabriel Marini <gabrielmarini12@icloud.com>
2025-03-19 22:43:50 -0300

    ajuste index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,10 @@ import routes from "./routes/routes"; // Importando as rotas
 
 const app = express();
 
-// Middleware para interpretar corpo da requisição como JSON
 app.use(express.json());
 
-// Usando as rotas importadas
-app.use("/api", routes); // Prefixo "/api" para todas as rotas
+app.use("/api", routes);
 
-// Iniciar o servidor
 app.listen(3000, () => {
   console.log("Servidor rodando na porta 3000");
 });
